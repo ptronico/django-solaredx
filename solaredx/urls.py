9d2e7ad7d3113e4b885cf31682bc950cbb3fbbdc
Pedro Vasconcelos <ptronico@gmail.com>
2013-11-25 10:41:02 -0300

    Modificando organização interna, inserindo chamada de Ping e melhorando chamadas de usuários.

--- solaredx/urls.py
+++ solaredx/urls.py
@@ -1,12 +1,16 @@
+# -*- coding: utf-8 -*-
 
+from django.conf import settings
 from django.conf.urls import patterns, url, include
 
 from tastypie.api import Api
 
-from .api.users import UserResource, UserProfileResource
+from .api.resources.ping import PingResource
+from .api.resources.users import UserResource, UserProfileResource
 
 
 dev_api = Api(api_name='dev')
+dev_api.register(PingResource())
 dev_api.register(UserResource())
 
 
@@ -18,3 +22,9 @@ urlpatterns = patterns('',
     url(r'^api/', include(test_urls, namespace='solaredx', app_name='solaredx')),
     url(r'^api/', include(dev_api.urls)),
 )
+
+# Solução para utilização do Django Debug Toolbar
+if settings.DEBUG:
+    urlpatterns += patterns('solaredx.views', 
+        url(r'^api/debug/', 'debug'),
+    )
--- solaredx/views.py
+++ solaredx/views.py
@@ -2,8 +2,32 @@
 
 import json
 
+from django.core import urlresolvers
 from django.http import HttpResponse
 
 
 def ping(request):
     return HttpResponse(json.dumps('pong'), content_type='application/json')
+
+
+# Solução para utilização do Django Debug Toolbar
+# Importante para análise de performance
+# http://www.marteinn.se/blog/?p=674
+def html_decorator(func):
+    def _decorated(*args, **kwargs):
+        response = func(*args, **kwargs)
+        wrapped = '<html><body>%s</html></body>' % response.content
+        # wrapped = ("<html><body>",response.content,"</body></html>")
+        return HttpResponse(wrapped)
+    return _decorated
+
+@html_decorator
+def debug(request):
+    path = request.META.get("PATH_INFO")
+    api_url = path.replace("debug/", "")
+    view = urlresolvers.resolve(api_url)
+    accept = request.META.get("HTTP_ACCEPT")
+    accept += ",application/json"
+    request.META["HTTP_ACCEPT"] = accept
+    res = view.func(request, **view.kwargs)
+    return HttpResponse(res._container)    
