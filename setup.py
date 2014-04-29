dda32c1a5785a59b5857a178f687e761f5c7bb4b
Andrei Bosco <andreibosco@gmail.com>
2014-04-29 13:35:05 -0300

    versão do módulo alterada

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-solaredx',
-    version='0.1',
+    version='0.1.1',
     packages=['solaredx'],
     include_package_data=True,
     license='BSD License',  # example license
